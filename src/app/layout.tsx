1cdc3e93033c0227068f324445af5ca011243034
dev-andreisava <dev.andreisava@gmail.com>
2024-01-03 22:57:26 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+export const metadata: Metadata = {
+  title: 'Sub Tracker',
+  description: 'Keep track of your subscriptions',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
